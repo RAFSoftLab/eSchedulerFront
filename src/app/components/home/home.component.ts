50e6084e3a8816a5d02d41189093c19ddd6d9852
Ivan Cicka <ivan.cicka.ic@gmail.com>
2024-12-02 08:43:46 +0100

    Add Distribution

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -27,12 +27,41 @@ export class HomeComponent implements OnInit {
   dataSource: MatTableDataSource<any>;
   isDistributionButtonDisabled: boolean = true;
 
+  summaryRows = 1;
+  totalLectures = 0;
+  totalExercises = 0;
+  weeklyLecturesE = 0;
+  weeklyExercisesE = 0;
+  weeklyLecturesO = 0;
+  weeklyExercisesO = 0;
+
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   teachers: Teacher[] = [];
   subjects: Subject[] = [];
   distributions: Distribution[] = [];
   selectedDistributions: Distribution[] = [];
+
+  columnNamesMap: { [key: string]: string } = {
+    firstName: 'Ime',
+    lastName: 'Prezime',
+    title: 'Zvanje',
+    name: 'Naziv',
+    studyProgram: 'Studijski program',
+    semester: 'Semestar',
+    countHours: 'Broj časova',
+    lectureHours: 'Fond predavanja',
+    exerciseHours: 'Fond vežbe',
+    practicumHours: 'Fond praktikum',
+    mandatory: 'Obaveznost',
+    lectureSessions: 'Predavanja (konsultacije)',
+    exerciseSessions: 'Vežbe (konsultacije)',
+    classType: 'Vrsta',
+    sessionCount:'Broj termina',
+    teacher: 'Nastavnik',
+    subject: 'Predmet'
+  };
 
 
   constructor(private teacherService: TeachersService, private subjectService: SubjectService, private distributionService: DistributionService) {
@@ -63,7 +92,7 @@ export class HomeComponent implements OnInit {
 
   showSubjects(): void {
     this.dataSource.data = this.subjects;
-    this.displayedColumns = ['name', 'studyProgram', 'semester', 'lectureHours', 'exerciseHours', 'practicumHours', 'mandatory', 'lectureSessions', 'exerciseSessions'];
+    this.displayedColumns = ['name', 'studyProgram', 'semester', 'lectureHours', 'exerciseHours', 'practicumHours', 'mandatory'];
     this.dataSource.paginator = this.paginator;
     this.isDistributionButtonDisabled = true;
   }
@@ -72,27 +101,84 @@ export class HomeComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  calculateSummaryRows() {
+    this.totalLectures = 0;
+    this.totalExercises = 0;
+
+    this.dataSource.data.forEach((row) => {
+      if(row.classType == "vezbe") {
+        this.totalExercises += row.sessionCount ;
+      }
+      else {
+        this.totalLectures += row.sessionCount;
+      }
+    });
+  }
+  calculateSummaryFromData() {
+    this.weeklyLecturesE = 0;
+    this.weeklyExercisesE = 0;
+    this.weeklyLecturesO = 0;
+    this.weeklyExercisesO = 0;
+    this.totalLectures = 0;
+    this.totalExercises = 0;
+
+    this.dataSource.data.forEach((row) => {
+
+      if(row.classType === "vezbe") {
+        this.totalExercises += (row.countHours *13* row.sessionCount);
+        if(row.semester % 2 == 0) {
+          this.weeklyExercisesE += row.countHours;
+        }else {
+          this.weeklyExercisesO += row.countHours;
+        }
+      }
+      else {
+        this.totalLectures += (row.countHours *13* row.sessionCount);
+        if(row.semester % 2 == 0) {
+          this.weeklyLecturesE += row.countHours;
+        }else {
+          this.weeklyLecturesO += row.countHours;
+        }
+      }
+    });
+  }
+
   onRowClicked(row: any) {
 
     if(row.hasOwnProperty('title')) {
+      this.summaryRows = 1;
       this.selectedDistributions = this.distributions.filter((distribution: { teacherId: any; }) => distribution.teacherId === row.id);
-      this.displayedColumns = ['teacher', 'subject', 'classType', 'sessionCount'];
+      this.displayedColumns = ['teacher','subject', 'studyProgram', 'semester', 'countHours','sessionCount' ,'classType'];
       this.dataSource.paginator = this.paginator;
     }else if (row.hasOwnProperty('name')){
-      this.displayedColumns = ['teacher', 'subject', 'classType', 'sessionCount'];
+      this.summaryRows = 2;
+      this.displayedColumns = ['teacher','subject', 'studyProgram', 'semester', 'countHours','sessionCount' ,'classType'];
       this.selectedDistributions = this.distributions.filter((distribution: { subjectId: any; }) => distribution.subjectId === row.id);
       this.dataSource.paginator = this.paginator;
     }
 
-    //Mapping data
-    this.dataSource.data = this.selectedDistributions.map((distribution) => ({
-      teacher: this.teachers.find((teacher) => teacher.id === distribution.teacherId)?.firstName + ' ' +
-        this.teachers.find((teacher) => teacher.id === distribution.teacherId)?.lastName,
-      subject: this.subjects.find((subject) => subject.id === distribution.subjectId)?.name,
-      classType: distribution.classType,
-      sessionCount: distribution.sessionCount,
-    }));
-
+    // Mapping data
+    this.dataSource.data = this.selectedDistributions.map((distribution) => {
+      const subject = this.subjects.find((subject) => subject.id === distribution.subjectId);
+      return {
+        teacher: this.teachers.find((teacher) => teacher.id === distribution.teacherId)?.firstName + ' ' +
+          this.teachers.find((teacher) => teacher.id === distribution.teacherId)?.lastName,
+        studyProgram: subject?.studyProgram,
+        semester: subject?.semester,
+        countHours: distribution.classType === 'vezbe'
+          ? subject?.exerciseHours
+          : subject?.lectureHours,
+        subject: subject?.name,
+        classType: distribution.classType,
+        sessionCount: distribution.sessionCount
+      };
+    });
+    if(this.summaryRows == 1){
+      this.calculateSummaryFromData();
+    }else if(this.summaryRows == 2){
+      this.calculateSummaryRows();
+    }
     // activate distribution button
     this.isDistributionButtonDisabled = this.selectedDistributions.length === 0;
   }
