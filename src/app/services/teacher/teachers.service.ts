09ab450da086cd4c271617e934a864929fcf437c
Ivan Cicka <ivan.cicka.ic@gmail.com>
2025-01-29 14:25:18 +0100

    chnge backend adress

--- src/app/services/teacher/teachers.service.ts
+++ src/app/services/teacher/teachers.service.ts
@@ -11,17 +11,23 @@ export class TeachersService {
   constructor(private httpClient:HttpClient) { }
 
   getTeachers(): Observable<Teacher[]>{
-    return this.httpClient.get<Teacher[]>('http://localhost:2525/api/teachers');
+    // return this.httpClient.get<Teacher[]>('http://localhost:2525/api/teachers');
+    return this.httpClient.get<Teacher[]>('/api/teachers');
+
   }
   saveTeacher(teacher: Teacher): Observable<Teacher>{
-    return this.httpClient.post<Teacher>('http://localhost:2525/api/teachers',teacher);
+    // return this.httpClient.post<Teacher>('http://localhost:2525/api/teachers',teacher);
+    return this.httpClient.post<Teacher>('api/teachers',teacher);
   }
   updateTeacher(teacher: Teacher): Observable<Teacher>{
-    return this.httpClient.put<Teacher>('http://localhost:2525/api/teachers',teacher);
+    // return this.httpClient.put<Teacher>('http://localhost:2525/api/teachers',teacher);
+    return this.httpClient.put<Teacher>('api/teachers',teacher);
   }
 
   deleteTeacher(teacherId: number): Observable<Teacher>{
-    return this.httpClient.delete<Teacher>(`http://localhost:2525/api/teachers/${teacherId}`);
+    // return this.httpClient.delete<Teacher>(`http://localhost:2525/api/teachers/${teacherId}`);
+    return this.httpClient.delete<Teacher>(`/api/teachers/${teacherId}`);
+
   }
 
 }
